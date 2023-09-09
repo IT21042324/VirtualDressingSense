a2d1af9dd39b97ef2a5d58a0f1312b0c2e45837c
M.C.M.A Sanjeevan <87469611+IT21023446@users.noreply.github.com>
2023-09-09 20:50:50 +0530

    Initial commit

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,45 @@
-import { SafeAreaView } from "react-native";
-import "react-native-gesture-handler";
-import Toast from "react-native-toast-message";
+import { SafeAreaView } from 'react-native';
+import 'react-native-gesture-handler';
+import Toast from 'react-native-toast-message';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Login, Signup, Welcome } from './screens/userManagement';
 
-import { globalStyles } from "./styles/global";
-import Navigator from "./routes/storeNavigator";
+import { globalStyles } from './styles/global';
+import Navigator from './routes/storeNavigator';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={globalStyles.container}>
       <Navigator />
       <Toast />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
